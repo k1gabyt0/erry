a8fc9f6ecbe3e712559928e1cc1afce944a99f12
Andrei Kigai <a.kigai@hey.com>
2022-11-15 08:37:19 +0300

    Move examples to godoc

--- error_example_test.go
+++ error_example_test.go
@@ -1,14 +1,13 @@
-package _example_test
+package erry_test
 
 import (
 	"errors"
 	"fmt"
+
 	"github.com/k1gabyt0/erry"
-	"testing"
 )
 
-func Example() {
-	// Create new error
+func ExampleNewError() {
 	errA := errors.New("err A")
 	errB := errors.New("err B")
 
@@ -16,32 +15,41 @@ func Example() {
 	// and errA and errB ass inner errors.
 	multierr := erry.NewError("multierror", errA, errB)
 	fmt.Println(multierr)
-	// Output: multierror:
-	//           err A
-	//           err B
+
 	if errors.Is(multierr, errA) {
 		fmt.Println("This is error A")
-		// Output: This is error A
+
 	}
 	if errors.Is(multierr, errB) {
 		fmt.Println("This is error B")
-		// Output: This is error B
-	}
 
-	// Transform existing error
+	}
+	// Output: multierror:
+	//	err A
+	//	err B
+	// This is error A
+	// This is error B
+}
+
+func ExampleErrorFrom() {
+	errA := errors.New("err A")
+	errB := errors.New("err B")
 
 	// Transforms errA into multi-error with errB
 	// as one of inner errors.
-	multierr = erry.ErrorFrom(errA, errB)
+	multierr := erry.ErrorFrom(errA, errB)
 	fmt.Println(multierr)
-	// Output: err A:
-	//		     err B
+
 	if errors.Is(multierr, errA) {
 		fmt.Println("This is error A")
-		// Output: This is error A
+
 	}
 	if errors.Is(multierr, errB) {
 		fmt.Println("This is error B")
-		// Output: This is error B
+
 	}
+	// Output: err A:
+	//	err B
+	// This is error A
+	// This is error B
 }
